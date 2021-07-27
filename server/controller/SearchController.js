eb0064cb9fe83d49a8640651342bf874c55a45a2
Bola Adebesin <bolaadebesin@bolas-air.myfiosgateway.com>
2021-07-27 08:46:56 -0400

    hotfix/reorganize client imports

--- server/controller/SearchController.js
+++ server/controller/SearchController.js
@@ -1,5 +1,5 @@
-const client_id = process.env.CLIENT_ID; 
-const client_secret = process.env.CLIENT_SECRET; 
+ const client_secret = process.env.CLIENT_SECRET; 
+ const client_id = process.env.CLIENT_ID;
  
 
 const axios = require('axios').default;
